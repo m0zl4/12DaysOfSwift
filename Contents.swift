ab927eb4ea4e076a12e0f462da88a50b5e396c17
m0zl4 <zayuka040104@gmail.com>
2021-03-16 13:37:16 +0700

    day 1

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+//Var
+var nama = "m0zla"
+nama = "latief"
+
+//Integer
+var harga = 10000
+harga = 1000
+
+var duit_jajan = 1_000_000
+duit_jajan = 20_000
+
+//Multi-Line String
+var belanja = """
+ roll bulu
+ kue sagu
+ pewangi lemari
+"""
+
